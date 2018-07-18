8b64bcd5096571aabfe1bd7def9fd8e5be3ea98a
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-18 14:32:18 -0400

    Added final page, which summarizes everything inputted from user, also adds three fav numbers together

--- personal_details_app.rb
+++ personal_details_app.rb
@@ -42,9 +42,9 @@ post '/personal_details_fav_numbers_page' do
 	fav_num2 = params[:fav_num2]
 	fav_num3 = params[:fav_num3]
 	erb :personal_details_fav_numbers_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], address_input: params[:address_input], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input]}
-	redirect '/final_page?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input + '&address_input=' + address_input + '&city_input=' + city_input + '&state_input=' + state_input + '&zip_input=' + zip_input + '&fav_num1=' + fav_num1 + '&fav_num2=' + fav_num2 + '&fav_num3=' + fav_num3
+	redirect '/personal_details_final_page?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input + '&address_input=' + address_input + '&city_input=' + city_input + '&state_input=' + state_input + '&zip_input=' + zip_input + '&fav_num1=' + fav_num1 + '&fav_num2=' + fav_num2 + '&fav_num3=' + fav_num3
 end
-get '/final_page' do
+get '/personal_details_final_page' do
 	first_name_input = params[:first_name_input]
 	last_name_input = params[:last_name_input]
 	address_input = params[:address_input]
@@ -54,5 +54,5 @@ get '/final_page' do
 	fav_num1 = params[:fav_num1]
 	fav_num2 = params[:fav_num2]
 	fav_num3 = params[:fav_num3]
-	erb :final_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], address_input: params[:address_input], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input], fav_num1: params[:fav_num1], fav_num2: params[:fav_num2], fav_num3: params[:fav_num3]}
+	erb :personal_details_final_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], address_input: params[:address_input], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input], fav_num1: params[:fav_num1], fav_num2: params[:fav_num2], fav_num3: params[:fav_num3]}
 end
