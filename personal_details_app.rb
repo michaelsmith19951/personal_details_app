1a9b8109e0f8d3a58096e021255e1a7a5d800eaf
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-18 14:14:41 -0400

    Added input for three favorite numbers, third page created

--- personal_details_app.rb
+++ personal_details_app.rb
@@ -20,39 +20,39 @@ post '/personal_details_physical_address_page' do
 	state_input = params[:state_input]
 	zip_input = params[:zip_input]
 	erb :personal_details_physical_address_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input]}
-	redirect '/fav_numbers?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input + '&physical_address=' + physical_address + '&city_input=' + city_input + '&state_input=' + state_input + '&zip_input=' + zip_input
+	redirect '/personal_details_fav_numbers_page?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input + '&address_input=' + address_input + '&city_input=' + city_input + '&state_input=' + state_input + '&zip_input=' + zip_input
 end
-get '/fav_numbers' do
+get '/personal_details_fav_numbers_page' do
 	first_name_input = params[:first_name_input]
 	last_name_input = params[:last_name_input]
 	address_input = params[:address_input]
 	city_input = params[:city_input]
 	state_input = params[:state_input]
 	zip_input = params[:zip_input]
-	erb :fav_numbers, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], physical_address: params[:physical_address], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input]}
+	erb :personal_details_fav_numbers_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], address_input: params[:address_input], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input]}
 end
-post '/fav_numbers' do
+post '/personal_details_fav_numbers_page' do
 	first_name_input = params[:first_name_input]
 	last_name_input = params[:last_name_input]
-	physical_address = params[:physical_address]
+	address_input = params[:address_input]
 	city_input = params[:city_input]
 	state_input = params[:state_input]
 	zip_input = params[:zip_input]
 	fav_num1 = params[:fav_num1]
 	fav_num2 = params[:fav_num2]
 	fav_num3 = params[:fav_num3]
-	erb :fav_numbers, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], physical_address: params[:physical_address], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input]}
-	redirect '/final_page?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input + '&physical_address=' + physical_address + '&city_input=' + city_input + '&state_input=' + state_input + '&zip_input=' + zip_input + '&fav_num1=' + fav_num1 + '&fav_num2=' + fav_num2 + '&fav_num3=' + fav_num3
+	erb :personal_details_fav_numbers_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], address_input: params[:address_input], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input]}
+	redirect '/final_page?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input + '&address_input=' + address_input + '&city_input=' + city_input + '&state_input=' + state_input + '&zip_input=' + zip_input + '&fav_num1=' + fav_num1 + '&fav_num2=' + fav_num2 + '&fav_num3=' + fav_num3
 end
 get '/final_page' do
 	first_name_input = params[:first_name_input]
 	last_name_input = params[:last_name_input]
-	physical_address = params[:physical_address]
+	address_input = params[:address_input]
 	city_input = params[:city_input]
 	state_input = params[:state_input]
 	zip_input = params[:zip_input]
 	fav_num1 = params[:fav_num1]
 	fav_num2 = params[:fav_num2]
 	fav_num3 = params[:fav_num3]
-	erb :final_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], physical_address: params[:physical_address], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input], fav_num1: params[:fav_num1], fav_num2: params[:fav_num2], fav_num3: params[:fav_num3]}
+	erb :final_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], address_input: params[:address_input], city_input: params[:city_input], state_input: params[:state_input], zip_input: params[:zip_input], fav_num1: params[:fav_num1], fav_num2: params[:fav_num2], fav_num3: params[:fav_num3]}
 end
